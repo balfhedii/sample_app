b2f6ada7e12f79c860d2df86e284c5fad99fd396
Perry Story <perry_story@yahoo.com>
2013-12-21 13:32:18 -0500

    Initialize repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby on Rails Tutorial: sample_app
+
+This is the sample for the [*Ruby on Rails Tutorial*](http://raistutorial.org)
+by [Michael Hartl](http://michaelhartl.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'cb0decdc41e3773948ebd2ac20d83cc5a4d060828d3fcbc32ac1c451e1940faef0a3a6be8462b4bbbb31e8297144e7bd980203e134ae099802c4c1634354b779'
